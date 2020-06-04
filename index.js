432ae0e0a0968b26fe9ac74daa41efb52c6d5433
Iman345 <60913360+Iman345@users.noreply.github.com>
2020-06-04 20:43:49 +0200

    javascript scrolling on page

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ console.log(1);
 const btn = document.querySelector('button');
 const backbtn = document.querySelector('backButton');
 
+
 btn.onclick = function() 
 {
     
@@ -20,3 +21,30 @@ btn.onclick = function()
 document.getElementById('backButton').addEventListener('click', () => {
   history.back();
 });
+
+
+
+
+
+
+var inserthere = $('h1');
+$('<nav class="scrollnav"><ul></ul></nav>').insertAfter(inserthere);
+var blogHeadings = $('article h2');
+
+blogHeadings.each(function(){
+var headingTxt = $(this).text();
+var listheading = '<li>' + headingTxt + '</li>'
+$('.scrollnav ul').append(listheading)});
+
+
+$('.scrollnav li').click(function(){
+ var index = $(this).index()
+ var articleheading = $( 'article h2:eq(' + index + ')' )
+ var pos = $(articleheading).offset().top - 10
+
+ $('html,body')
+
+    .animate({scrollTop: pos});
+});
+
+
