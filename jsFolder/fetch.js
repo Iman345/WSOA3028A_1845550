c8dc281148d3a95338f1f07c5a93a219ea5d2de0
Iman345 <60913360+Iman345@users.noreply.github.com>
2020-06-28 12:34:11 +0200

    fetch

--- jsFolder/fetch.js
+++ jsFolder/fetch.js
@@ -1,4 +1,4 @@
-fetch('https://reqres.in/api/users')
+/*fetch('https://reqres.in/api/users')
 .then((r)=>r.json())
 .then((names)=>{console.log(names);
 
@@ -7,5 +7,15 @@ handlenames(names)
 
 const handlenames =(namedetails)=>{
     document.querySelector('p').innerText= namedetails.data;
-}
-   
+}*/
+
+fetch('https://reqres.in/api/users')
+   .then(res => res.json())
+   .then(res => {
+       res.data.map(user => {
+           console.log(`${user.id}: ${user.first_name} ${user.last_name}`);
+       });
+   });
+   const handlenames =(namedetails)=>{
+    document.querySelector('p').innerText= namedetails.data;
+};
