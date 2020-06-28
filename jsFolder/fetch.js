bba513306624897a48b317e00673318a4972913f
Iman345 <60913360+Iman345@users.noreply.github.com>
2020-06-28 12:21:04 +0200

    fetch

--- jsFolder/fetch.js
+++ jsFolder/fetch.js
@@ -1,7 +1,4 @@
 fetch('https://reqres.in/api/users')
-   .then(res => res.json())
-   .then(res => {
-       res.data.map(user => {
-           console.log(`${user.id}: ${user.first_name} ${user.last_name}`);
-       });
-   });
+.then((r)=>r.json())
+.then((p)=>console.log(p));
+   
