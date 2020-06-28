9f396c019d3b478ee9f15ace71f7e31fd48ae05c
Iman345 <60913360+Iman345@users.noreply.github.com>
2020-06-28 18:47:52 +0200

    Update fetch.js

--- jsFolder/fetch.js
+++ jsFolder/fetch.js
@@ -72,50 +72,28 @@ document.querySelector(".quote").innerText = quranDetails.data.text;
     const handlequote = (quoteDetails) => {
         document.querySelector("p").innerText = quoteDetails.content+"-"+ quoteDetails.author;
         };*/
-  
-       
-       
-       
-       
-        fetch('https://api.quotable.io/random')
-    .then(res => res.json())
-    .then(quote => {
-      console.log(`${quote.content} —${quote.author}`)
-      handlequote(quote)
-    })
-    
-    const handlequote = (quoteDetails) => {
-        document.querySelector("p").innerText = quoteDetails.content+"-"+ quoteDetails.author;
-        };
-
-
-
-        document.addEventListener("DOMContentLoaded", () => {
-         
-            const quotebutton = document.querySelector("button");
-            const quotetxt = document.querySelector("blockquote p");
-            const author = document.querySelector("blockquote cite");
           
-            async function changeQuote() {
+            function changeQuote() {
               
-              const res= await fetch("https://api.quotable.io/random");
-              const info = await res.json();
-              if (res.ok) {
-              
-                quotetxt.textContent = data.content;
-                author.textContent = data.author;
-              } else {
-                quotetxt.textContent = "An error occured";
-                console.log(info);
+                fetch('https://api.quotable.io/random')
+                .then(res => res.json())
+                .then(quote => {
+                  console.log(`${quote.content} —${quote.author}`)
+                  handlequote(quote)
+                })
+                
+                const handlequote = (quoteDetails) => {
+                    document.querySelector("p").innerText = quoteDetails.content+"-"+ quoteDetails.author;
+                    };
               }
-            }
+            
           
            
             button.addEventListener("click", changeQuote);
           
            
             changeQuote();
-          });
+    
 
 
 
